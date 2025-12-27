9c6453208856a1851b481c0da051e3e8a0de445d
Lucas Seyoum <lkseyoum@gmail.com>
2025-12-27 16:28:37 -0500

    feat: update navbar logo to use formlab-logo.png
    - Replace logo.svg with formlab-logo.png in navbar
    - Set logo size to 32x32 on desktop (md:w-8 md:h-8)
    - Set logo size to 28x28 on mobile (w-7 h-7)
    - Remove references to old logo.svg from navbar
    - Use Next.js Image component for optimized loading

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,7 +8,13 @@ export const Navbar: React.FC = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           <Link href="/" className="flex items-center space-x-2">
-            <Image src="/logo.svg" alt="FormLab" width={48} height={56} className="object-contain" />
+            <Image 
+              src="/formlab-logo.png" 
+              alt="FormLab" 
+              width={32} 
+              height={32} 
+              className="object-contain w-8 h-8 md:w-8 md:h-8 w-7 h-7" 
+            />
             <span className="text-xl font-bold text-white">FormLab</span>
           </Link>
           <div className="flex space-x-4">
